ae3b9ca54c6c96d8a94bdd23b3d8fde93079ba03
Milen Radkov <milenradkov@me.com>
2019-08-02 09:43:19 +0300

    wip on tests

--- test/waellet-tipTest.js
+++ test/waellet-tipTest.js
@@ -15,22 +15,71 @@
  *  PERFORMANCE OF THIS SOFTWARE.
  */
 
+const Ae = require('@aeternity/aepp-sdk').Universal;
+const Crypto = require('@aeternity/aepp-sdk').Crypto;
+const Bytes = require('@aeternity/aepp-sdk/es/utils/bytes');
+var blake2b = require('blake2b');
+
+const config = {
+    host: 'http://localhost:3001/',
+    internalHost: 'http://localhost:3001/internal/',
+    compilerUrl: 'http://localhost:3080'
+};
+
 
 const Deployer = require('forgae-lib').Deployer;
 const WAELLET_CONTRACT_PATH = "./contracts/waellet-tip.aes";
 
 describe('Waellet Tip Contract', () => {
 
-    let deployer;
-    let ownerKeyPair = wallets[0];
-    
+    let ownerKeypair, otherKeypair, owner, otherClient, contract;
+
     before(async () => {
-        deployer = new Deployer('local', ownerKeyPair.secretKey)
-    })
+        ownerKeypair = wallets[0];
+        owner = await Ae({
+            url: config.host,
+            internalUrl: config.internalHost,
+            keypair: ownerKeypair,
+            nativeMode: true,
+            networkId: 'ae_devnet',
+            compilerUrl: config.compilerUrl
+        });
 
-    it('Deploying Example Contract', async () => {
-        const deployPromise = deployer.deploy(WAELLET_CONTRACT_PATH) // Deploy it
+        otherKeypair = wallets[1];
+        otherClient = await Ae({
+            url: config.host,
+            internalUrl: config.internalHost,
+            keypair: otherKeypair,
+            nativeMode: true,
+            networkId: 'ae_devnet',
+            compilerUrl: config.compilerUrl
+        });
+    });
 
-        await assert.isFulfilled(deployPromise, 'Could not deploy the Waellet Tip Smart Contract'); // Check whether it's deployed
-    })
+    const hashTopic = topic => blake2b(32).update(Buffer.from(topic)).digest('hex');
+    const topicHashFromResult = result => Bytes.toBytes(result.result.log[0].topics[0], true).toString('hex');
+
+    beforeEach(async () => {
+        let contractSource = utils.readFileRelative(WAELLET_CONTRACT_PATH, 'utf-8');
+        contract = await owner.getContractInstance(contractSource);
+        const contractInstance = await contract.deploy([]);
+        assert.equal(contractInstance.result.returnType, 'ok');
+    });
+
+    it('Deploying Waellet Tip Contract', async () => {
+        const contractInstance = await contract.deploy([]);
+        assert.equal(contractInstance.result.returnType, 'ok');
+    });
+
+    it('Should Claim domain ownership', async () => {
+        let contractSource = utils.readFileRelative(WAELLET_CONTRACT_PATH, 'utf-8');
+        let deployTestContract = await owner.getContractInstance(contractSource);
+
+        const deploy = await deployTestContract.deploy([]);
+        assert.equal(deploy.result.returnType, 'ok');
+
+        const claim = await deployTestContract.methods.claim('hack.bg').catch(e => e);
+        console.log(claim);
+
+    });
 })
